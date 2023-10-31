99fddfd19d11a341aeba53b0230940fdf9404adf
AFTAB SHAIK <shaikaftab02@gmail.com>
2023-10-31 23:51:47 +0530

    Create Rotate_Image.java

---
+++ Arrays/Arrays_Medium/Rotate_Image.java
@@ -0,0 +1,44 @@
+package Arrays.medium_problems;
+
+import java.util.*;
+
+public class Rotate_Image {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        rotate(matrix);
+
+        System.out.println("Output: ");
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void rotate(int[][] matrix) {
+        if (matrix.length == 0) return;
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+                // top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+    }
+}
