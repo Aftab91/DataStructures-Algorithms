6be66aef57073b8914435eb567502f21a0774c59
AFTAB SHAIK <shaikaftab02@gmail.com>
2023-11-10 23:52:54 +0530

    Create MaximumPoints_you_can_Obtain_from_Cards.java

---
+++ Arrays/Arrays_Medium/MaximumPoints_you_can_Obtain_from_Cards.java
@@ -0,0 +1,25 @@
+package Arrays.medium_problems;
+
+public class MaximumPoints_you_can_Obtain_from_Cards {
+    public static int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length, totalSum = 0, minSubarray = 0;
+
+	    for(int i=0, currSum=0; i<n; i++) {
+		    totalSum += cardPoints[i];
+		    currSum += cardPoints[i];
+		    if (i < n-k) minSubarray += cardPoints[i];
+		    else {
+			currSum -= cardPoints[i-(n-k)];
+			minSubarray = Math.min(minSubarray, currSum);
+		    }
+	    }
+	    return totalSum - minSubarray;
+    }
+    
+    public static void main(String[] args) {
+        int[] cardPoints = {1, 2, 3, 4, 5, 6, 1};
+        int k = 3;
+        int result = maxScore(cardPoints, k);
+        System.out.println("Maximum score: " + result);
+    }
+}
