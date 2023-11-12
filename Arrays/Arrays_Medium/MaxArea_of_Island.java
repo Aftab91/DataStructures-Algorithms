ad5a4622e11ccc7335c5657b745424c0452075e7
AFTAB SHAIK <shaikaftab02@gmail.com>
2023-11-12 21:30:28 +0530

    Create MaxArea_of_Island.java

---
+++ Arrays/Arrays_Medium/MaxArea_of_Island.java
@@ -0,0 +1,47 @@
+package Arrays.medium_problems;
+
+public class MaxArea_of_Island {
+    
+	public int dfs(int i, int j, int[][] grid){
+        grid[i][j]=2;
+        int c = 1;
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        for(int[] d : directions){
+            int new_x = i+d[0];
+            int new_y = j+d[1];
+            if(new_x>=0 && new_x<grid.length && new_y>=0 && new_y<grid[0].length && grid[new_x][new_y]==1){
+                c+=dfs(new_x, new_y, grid);
+            }
+        }
+        return c;
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]==1){
+                    int area = dfs(i, j, grid);
+                    max = Math.max(max,area);
+                }
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        MaxArea_of_Island solution = new MaxArea_of_Island();
+        int[][] grid = {
+            {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        
+        int maxArea = solution.maxAreaOfIsland(grid);
+        System.out.println("Maximum area of the island: " + maxArea);
+    }
+}
